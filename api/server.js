734bd9504005528731becaecfd2c1c27cceddd4b
jsfanatik <jsfanatik@gmail.com>
2020-01-20 13:22:59 -0500

    removed mongoose.Promise

--- api/server.js
+++ api/server.js
@@ -7,7 +7,6 @@ const mongoose = require('mongoose');
 const config = require('./DB.js');
 const postRoute = require('./post.route');
 
-mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
   () => { console.log('Database is connected') },
   err => { console.log('Can not connect to the database'+ err)}
